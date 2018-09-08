2faee73488fc089c6dabcff6244c5002734d00f3
Kyle Crawford <kcrwfrd@gmail.com>
2018-09-08 01:59:53 -0400

    Update setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Link VMware AirWatch Munki log to standard location for DEPNotify
+# Create and Link VMware AirWatch Munki log to standard location for DEPNotify
 /bin/mkdir -p '/Library/Managed Installs/Logs'
+/bin/mkdir -p '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Logs/'
+/usr/bin/touch '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Logs/ManagedSoftwareUpdate.log'
 /bin/ln -s '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Logs/ManagedSoftwareUpdate.log' '/Library/Managed Installs/Logs/ManagedSoftwareUpdate.log'
