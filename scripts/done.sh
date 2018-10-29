df1755958aee13aa93779241ef8f2cdcd3624eda
Kyle Crawford <kcrwfrd@gmail.com>
2018-10-29 15:13:25 -0400

    Add Outlook to Dock

--- scripts/done.sh
+++ scripts/done.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+/usr/local/bin/dockutil --add /Applications/Microsoft\ Outlook.app --replacing "Mail"
 echo "Status: " >> /var/tmp/depnotify.log
 echo "Command: MainTitle: Device is Ready for Use" >>/var/tmp/depnotify.log
 echo "Command: MainText: \nConfiguration of your macOS device is complete.\n\nLogin to the Workspace ONE app to install additional applications." >>/var/tmp/depnotify.log
