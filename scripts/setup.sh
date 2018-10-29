1a7045078bf1a7f25d1a28e4ae2320e6fb40bbc5
Kyle Crawford <kcrwfrd@gmail.com>
2018-10-29 15:13:48 -0400

    Ensure depnotify log is writable in userland

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Ensure depnotify file is writable in userland
+/usr/bin/touch /var/tmp/depnotify.log
+/bin/chmod 666 /var/tmp/depnotify.log
+
 # Create and Link VMware AirWatch Munki log to standard location for DEPNotify
 /bin/mkdir -p '/Library/Managed Installs/Logs'
 /bin/mkdir -p '/Library/Application Support/AirWatch/Data/Munki/Managed Installs/Logs/'
