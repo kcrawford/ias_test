4245ca3be4b66089934fd4255d9947dc6923a989
Kyle Crawford <kcrwfrd@gmail.com>
2018-10-29 14:13:04 -0400

    Update completion

--- scripts/done.sh
+++ scripts/done.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "Status: Waiting for Software Manifest from AirWatch..." >> /var/tmp/depnotify.log
+echo "Status: " >> /var/tmp/depnotify.log
+echo "Command: MainTitle: Device is Ready for Use" >>/var/tmp/depnotify.log
+echo "Command: MainText: \nConfiguration of your macOS device is complete.\n\nLogin to the Workspace ONE app to install additional applications." >>/var/tmp/depnotify.log
+echo "Command: ContinueButton: Close" >> /var/tmp/depnotify.log
